6aa6c69bc20a4c2dc63c04263ff40162629194d2
AlexiWolf <alexiwolf@pm.me>
2021-03-25 21:15:18 -0500

    Test for howler namespace on api routes

---
+++ tests/howler/test_howler_urls.py
@@ -0,0 +1,30 @@
+#  Copyright (C) 2021 AlexiWolf
+#
+#  Howler is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  Howler is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Howler.  If not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+from django.urls import reverse
+from mixer.backend.django import mixer
+
+from howler.models import Post
+
+
+@pytest.fixture()
+def post() -> Post:
+    return mixer.blend(Post)
+
+
+@pytest.mark.django_db
+def test_should_use_howler_namespace(post: Post):
+    assert reverse("howler:api-root") == f"/"
