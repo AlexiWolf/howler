6d7986033e89df4b3cd1a2d6d83aff345233f07a
AlexiWolf <alexiwolf@pm.me>
2021-04-12 21:42:01 -0500

    The Post list is visible to anonymous users

--- tests/howler/test_post_viewset.py
+++ tests/howler/test_post_viewset.py
@@ -13,3 +13,41 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with Howler.  If not, see <https://www.gnu.org/licenses/>.
 
+import pytest
+from django.contrib.auth import get_user_model
+from django.core.handlers.wsgi import WSGIRequest
+from mixer.backend.django import mixer
+from rest_framework.test import APIRequestFactory
+
+from howler.models import Post
+from howler.viewsets import PostViewSet
+
+UserModel = get_user_model()
+
+
+@pytest.fixture()
+def post() -> Post:
+    return mixer.blend(Post)
+
+
+@pytest.fixture()
+def api_request_factory() -> APIRequestFactory:
+    return APIRequestFactory()
+
+
+@pytest.fixture()
+def get_request(api_request_factory: APIRequestFactory):
+    return api_request_factory.get("/fake-url/")
+
+
+@pytest.fixture()
+def view_set() -> PostViewSet:
+    return PostViewSet
+
+
+@pytest.mark.django_db
+def test_should_allow_anonymous_users_to_view_post_list(
+        get_request: WSGIRequest, view_set: PostViewSet):
+    view = view_set.as_view({"get": "list"})
+    response = view(get_request)
+    assert response.status_code == 200
